204f051d431f0f85d065084bc8757cba22b13c46
Sami00002 <samuel.bodogan@emanuel.ro>
2022-01-06 13:01:11 +0200

    Add project files.

---
+++ Recursivitate - 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Recursivitate___1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cate numeri vrei sa citesti de la tastatura?");
+            int nrElements = int.Parse(Console.ReadLine());
+            int[] array = new int[nrElements];
+            for (int i = 0; i < nrElements; i++)
+            {
+                Console.Write("The " + (i + 1) + ". element = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(array);
+        }
+    }
+}
