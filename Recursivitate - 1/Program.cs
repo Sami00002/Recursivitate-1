94965603e3bc1ffd4e980bacd1dc4acea69f542c
Sami00002 <samuel.bodogan@emanuel.ro>
2022-01-13 23:31:38 +0200

    .

--- Recursivitate - 1/Program.cs
+++ Recursivitate - 1/Program.cs
@@ -4,17 +4,32 @@ namespace Recursivitate___1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void ParcurgereVector(int[] array, int n)
         {
-            Console.Write("Cate numeri vrei sa citesti de la tastatura?");
-            int nrElements = int.Parse(Console.ReadLine());
-            int[] array = new int[nrElements];
-            for (int i = 0; i < nrElements; i++)
+            if (n == 1)
+                Console.WriteLine(array[n - 1]);
+            else
             {
-                Console.Write("The " + (i + 1) + ". element = ");
-                array[i] = int.Parse(Console.ReadLine());
+
+                ParcurgereVector(array, n - 1);
+                Console.WriteLine(array[n - 1]);
             }
-            Console.WriteLine(array);
         }
+
+                static void Main(string[] args)
+                {
+                         Console.Write("Cate numeri vrei sa citesti de la tastatura?");
+                         int nrElements = int.Parse(Console.ReadLine());
+                         int[] array = new int[nrElements];
+                         for (int i = 0; i < nrElements; i++)
+                         {
+                         Console.Write("The " + (i + 1) + ". element = ");
+                         array[i] = int.Parse(Console.ReadLine());
+                         }
+
+                         ParcurgereVector(array, array.Length);
+                }
+            
+    
     }
 }
